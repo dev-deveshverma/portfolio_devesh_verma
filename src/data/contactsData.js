f9abed6c70c819a4ed836c5e5063e04e041de2b8
DEVESH VERMA <71995793+webdeveshverma@users.noreply.github.com>
2022-08-08 19:23:52 +0530

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'webdeveshverma@gmail.com',
     phone: '+91 7060596109',
-    address: 'Harsaon Shastri Nagar Ghaziabad',
+    address: 'Shastri Nagar Ghaziabad',
 
     sheetAPI: 'https://sheetdb.io/api/v1/tt5b8x0x5sbmo'
 }
