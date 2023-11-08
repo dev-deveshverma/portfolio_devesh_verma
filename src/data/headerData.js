2b92f69647810d850cc2b9b74ced74a1fca0f295
DEVESH VERMA <71995793+dev-deveshverma@users.noreply.github.com>
2023-11-09 01:02:44 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/devesh verma resume.pdf'
 export const headerData = {
     name: 'Devesh Verma',
     title: "Full-Stack Web Developer",
-    desciption:"I'm a Full-Stack Web Developer with a passion for building beautiful and functional websites. I have experience in building websites and web applications using JavaScript, React, Node.js, Express, MongoDB, HTML, CSS, and Bootstrap. I am currently looking for a full-time position as a Full-Stack Web Developer.",
+    desciption:"I'm a Full-Stack Web Developer with a passion for building beautiful and functional websites. I have experience in building websites and web applications using JavaScript, React, Node.js, Express, MongoDB, HTML, CSS, and Bootstrap.",
     image: 'https://i.ibb.co/PrNM8SN/Devesh-modified.png',
     resumePdf: resume
 }
