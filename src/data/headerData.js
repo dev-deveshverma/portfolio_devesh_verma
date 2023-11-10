d0ddcafa1cd4a35e2d23d97b91594d7321448ca4
devesh.webpatriot <devesh.webpatriot@gmail.com>
2023-11-10 16:56:41 +0530

    fix bugs

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Devesh Verma',
     title: "Full-Stack Web Developer",
     desciption:"I'm a Full-Stack Web Developer with a passion for building beautiful and functional websites. I have experience in building websites and web applications using JavaScript, React, Node.js, Express, MongoDB, HTML, CSS, and Bootstrap.",
-    image: 'https://i.ibb.co/zxkj0kg/IMG-20230916-051609-474.jpg',
+    image: 'https://i.ibb.co/PrNM8SN/Devesh-modified.png',
     resumePdf: resume
 }
