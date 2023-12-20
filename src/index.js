829620ade8b386dcba9828d00bb6447abfca4a95
devesh.webpatriot <devesh.webpatriot@gmail.com>
2023-12-20 15:22:16 +0530

    fixed google analytics

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ThemeContextProvider from './contexts/ThemeContext'
-
-
+import ReactGA  from 'react-ga';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+
+const Tracking_Id= "G-BQ2TJFZ2Y5";
+ReactGA.initialize(Tracking_Id);
 
 ReactDOM.render(
     <ThemeContextProvider>
