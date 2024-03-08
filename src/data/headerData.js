cf80cf0e38b6b35fae0539549a2e83e9945e6822
DEVESH VERMA <71995793+dev-deveshverma@users.noreply.github.com>
2024-03-08 14:37:38 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Devesh Verma',
     title: "Full-Stack Web Developer",
     desciption:"I'm a Full-Stack Web Developer with a passion for building beautiful and functional websites. I have experience in building websites and web applications using JavaScript, React, Node.js, Express, MongoDB, HTML, CSS, and Bootstrap.",
-    image: 'https://i.ibb.co/PrNM8SN/Devesh-modified.png',
+    image: 'https://i.ibb.co/TW2L73J/Whats-App-Image-2024-03-08-at-10-18-58-ea0a64b4-removebg-preview-1-removebg-preview.png',
     resumePdf: resume
 }
