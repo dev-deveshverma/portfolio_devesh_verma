7cc3b9f50f9a2dd1b81dbfdadfd26db252ae5682
DEVESH VERMA <71995793+dev-deveshverma@users.noreply.github.com>
2024-03-08 14:24:37 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -32,7 +32,7 @@ export const projectsData = [
     },
     {
         id: 1,
-        projectName: 'Employee Management',
+        projectName: 'CAD BULL',
         projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
         tags: ['React', 'CSS', 'Material Ui'],
         code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
