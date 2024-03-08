f0a49b01df510f8a01c7c574441a9bf5ce030e47
DEVESH VERMA <71995793+dev-deveshverma@users.noreply.github.com>
2024-03-08 14:17:08 +0530

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,14 +1,7 @@
 export const socialsData = {
     github: 'https://github.com/dev-deveshverma',
-    facebook: 'https://www.facebook.com/dev-deveshverma',
     linkedIn: 'https://www.linkedin.com/in/dev-deveshverma',
     instagram: 'https://www.instagram.com/dev_deveshverma',
     codepen: 'https://codepen.io/dev-deveshverma',
-    twitter: 'https://twitter.com/dev_deveshverma',
-    reddit: 'https://www.reddit.com/user/dev-deveshverma',
-    blogger: 'https://www.blogger.com/dev-deveshverma',
     medium: 'https://medium.com/@dev-deveshverma',
-    stackOverflow: 'https://stackoverflow.com/users/dev-deveshverma',
-    gitlab: 'https://gitlab.com/dev-deveshverma',
-    youtube: 'https://youtube.com/dev-deveshverma'
 }
